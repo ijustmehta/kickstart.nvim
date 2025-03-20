8a3e4a47380d736348a7252b153d58d756be4720
Rahul Mehta <ijustmehta@gmail.com>
2025-03-21 00:11:34 +0530

    upd: added neoscroll plugin

--- init.lua
+++ init.lua
@@ -980,6 +980,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins.leetcode' },
+  { import = 'custom.plugins.neoscroll' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
