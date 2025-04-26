e7566b7ff911c3baa978321960180bded4effe04
Rahul Mehta <ijustmehta@gmail.com>
2025-04-26 13:49:25 +0530

    upd: made init.lua lean

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -35,3 +35,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
